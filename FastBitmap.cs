026d0ac9747809478308c6afb3d6cd0d3dd808ae
kard02 <karolkasperek02@gmail.com>
2020-03-12 20:45:01 +0100

    Initial commit

---
+++ FastBitmap.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MGI.FastImaging
+{
+    /// <summary>
+    /// Encapsulates <see cref="Bitmap"/> for better <c>GetPixel</c> and <c>SetPixel</c> performance. 
+    /// Uses <see cref="PixelFormat.Format32bppArgb"/> internally.
+    /// </summary>
+    public class FastBitmap : IDisposable
+    {
+        /// <summary>
+        /// Gets the width in pixels.
+        /// </summary>
+        public int Width { get => bitmap.Width; }
+        /// <summary>
+        /// Gets the height in pixels.
+        /// </summary>
+        public int Height { get => bitmap.Height; }
+        /// <summary>
+        /// Gets the width and height in pixels.
+        /// </summary>
+        public Size Size { get => bitmap.Size; }
+
+        private Bitmap bitmap;
+        private BitmapData bitmapData;
+        private IntPtr scan0;
+        private int stride;
+        private bool lockedBits;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastBitmap"/> class from the specified existing bitmap.
+        /// </summary>
+        /// <param name="original">The <see cref="Bitmap"/> from which to create the new bitmap. Does not need to be disposed if consumed.</param>
+        /// <param name="consume">When false copies specified <see cref="Bitmap"/> instead of storing provided one.</param>
+        public FastBitmap(Bitmap original, bool consume = true)
+        {
+            Bitmap clone = new Bitmap(original.Width, original.Height, PixelFormat.Format32bppArgb);
+            Bitmap target = consume ? original : new Bitmap(original);
+            original.Dispose();
+            
+            // Converting bitmap to argb format
+            using (Graphics graphics = Graphics.FromImage(clone))
+                graphics.DrawImage(target, new Rectangle(0, 0, clone.Width, clone.Height));
+
+            bitmap = target;
+            LockBits();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastBitmap"/> class with the specified size.
+        /// </summary>
+        /// <param name="width">The width in pixels.</param>
+        /// <param name="height">The height in pixels.</param>
+        public FastBitmap(int width, int height) : this(new Bitmap(width, height)) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastBitmap"/> class from the specified file.
+        /// </summary>
+        /// <param name="filename">The bitmap file name and path.</param>
+        public FastBitmap(string filename) : this(new Bitmap(filename)) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastBitmap"/> class from the specified data stream.
+        /// </summary>
+        /// <param name="stream">The data stream used to load the image.</param>
+        public FastBitmap(Stream stream) : this(new Bitmap(stream)) { }
+
+        /// <summary>
+        /// Bits must be locked for using <see cref="GetPixel"/> and <see cref="SetPixel"/> methods. This method is called by the constructor.
+        /// </summary>
+        public void LockBits()
+        {
+            if (!lockedBits)
+            {
+                bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                    ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                scan0 = bitmapData.Scan0;
+                stride = bitmapData.Stride;
+                lockedBits = true;
+            }
+        }
+
+        /// <summary>
+        /// Get underlying <see cref="Bitmap"/> object. Use <see cref="LockBits"/> method to lock bits again.
+        /// </summary>
+        /// <returns>Encapsulated bitmap.</returns>
+        public Bitmap UnlockBits()
+        {
+            if (lockedBits)
+                bitmap.UnlockBits(bitmapData);
+
+            lockedBits = false;
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Gets the color of the specified pixel in this <see cref="FastBitmap"/>.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the pixel to retrieve.</param>
+        /// <param name="y">The y-coordinate of the pixel to retrieve.</param>
+        /// <returns>Color of the pixel.</returns>
+        public FastColor GetPixel(int x, int y) => new FastColor(GetPixelPosition(x, y));
+        /// <summary>
+        /// Sets the color of the specified pixel in this <see cref="FastBitmap"/>.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the pixel to set.</param>
+        /// <param name="y">The y-coordinate of the pixel to set.</param>
+        /// <param name="fastColor">The color to assign to the specified pixel.</param>
+        public void SetPixel(int x, int y, FastColor fastColor) => Marshal.WriteInt32(GetPixelPosition(x, y), fastColor.ToInt());
+
+        private IntPtr GetPixelPosition(int x, int y) => scan0 + (x * 4 + y * stride);
+
+        #region IDisposable
+        private bool disposedValue = false; // To detect redundant calls
+
+        /// <summary>
+        /// Releases all resources used by this <see cref="FastBitmap"/>.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                    bitmap.Dispose();
+
+                bitmapData = null;
+                disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Deconstructor.
+        /// </summary>
+        ~FastBitmap()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases all resources used by this <see cref="FastBitmap"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ FastColor.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace MGI.FastImaging
+{
+    // TODO: Consider rewriting, not storing data in remote memory location
+
+    /// <summary>
+    /// Stores an ARGB color as a pointer in BGRA order. Used by <see cref="FastBitmap"/>.
+    /// </summary>
+    public class FastColor
+    {
+        /// <summary>
+        /// Pointer to the <see cref="FastColor"/> data.
+        /// </summary>
+        public readonly IntPtr ptr;
+
+        /// <summary>
+        /// The alpha component value of this <see cref="FastColor"/>.
+        /// </summary>
+        public byte A { get => Marshal.ReadByte(ptr, 3); set => Marshal.WriteByte(ptr, 3, value); }
+        /// <summary>
+        /// The red component value of this <see cref="FastColor"/>.
+        /// </summary>
+        public byte R { get => Marshal.ReadByte(ptr, 2); set => Marshal.WriteByte(ptr, 2, value); }
+        /// <summary>
+        /// The green component value of this <see cref="FastColor"/>.
+        /// </summary>
+        public byte G { get => Marshal.ReadByte(ptr, 1); set => Marshal.WriteByte(ptr, 1, value); }
+        /// <summary>
+        /// The blue component value of this <see cref="FastColor"/>.
+        /// </summary>
+        public byte B { get => Marshal.ReadByte(ptr, 0); set => Marshal.WriteByte(ptr, 0, value); }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FastColor"/> class by copying conetents of the specified pointer. Use BGRA order.
+        /// </summary>
+        /// <param name="ptr">Pointer to a BGRA color. Instance will not point to this memory address.</param>
+        public FastColor(IntPtr ptr) : this(Marshal.ReadInt32(ptr)) { }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FastColor"/> class from specified <see cref="int"/>. Use BGRA order.
+        /// </summary>
+        /// <param name="bytes">4 bytes long integer in BGRA order.</param>
+        public FastColor(int bytes)
+        {
+            ptr = Marshal.AllocHGlobal(4);
+            Marshal.WriteInt32(ptr, bytes);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FastColor"/> class with specified values.
+        /// </summary>
+        /// <param name="a">The alpha component value.</param>
+        /// <param name="r">The red component value.</param>
+        /// <param name="g">The green component value.</param>
+        /// <param name="b">The blue component value.</param>
+        public FastColor(byte a, byte r, byte g, byte b)
+        {
+            ptr = Marshal.AllocHGlobal(4);
+            A = a;
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="FastColor"/> class from specified <see cref="Color"/>.
+        /// </summary>
+        /// <param name="color"><see cref="Color"/> value.</param>
+        public FastColor(Color color) : this(color.A, color.R, color.G, color.B) { }
+
+        /// <summary>
+        /// Returns an <see cref="int"/> representation of that <see cref="FastColor"/>.
+        /// </summary>
+        /// <returns><see cref="int"/> representation of that <see cref="FastColor"/>.</returns>
+        public int ToInt() => Marshal.ReadInt32(ptr);
+
+        /// <summary>
+        /// Cast <see cref="FastColor"/> to <see cref="Color"/>.
+        /// </summary>
+        /// <param name="color"><see cref="FastColor"/> to cast.</param>
+        public static explicit operator Color(FastColor color) => Color.FromArgb(color.A, color.R, color.G, color.B);
+        
+        /// <summary>
+        /// Cast <see cref="Color"/> to <see cref="FastColor"/>.
+        /// </summary>
+        /// <param name="color"><see cref="Color"/> to cast.</param>
+        public static explicit operator FastColor(Color color) => new FastColor(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts this <see cref="FastColor"/> structure to a human-readable string.
+        /// </summary>
+        /// <returns>String representation of this <see cref="FastColor"/></returns>
+        public override string ToString() => $"[{A}, {R}, {G}, {B}]";
+
+        /// <summary>
+        /// Returns a hash code for this <see cref="FastColor"/>.
+        /// </summary>
+        /// <returns>Hash code for this <see cref="FastColor"/></returns>
+        public override int GetHashCode()
+        {
+            var hashCode = -1749689076;
+            hashCode = hashCode * -1521134295 + A.GetHashCode();
+            hashCode = hashCode * -1521134295 + R.GetHashCode();
+            hashCode = hashCode * -1521134295 + G.GetHashCode();
+            hashCode = hashCode * -1521134295 + B.GetHashCode();
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Tests whether the specified object is a <see cref="FastColor"/> class and is equivalent to this <see cref="FastColor"/>.
+        /// </summary>
+        /// <param name="obj">The object to test.</param>
+        /// <returns>Returns true if test passes.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is FastColor color)
+                return ToInt() == color.ToInt();
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Tests if <see cref="FastColor"/>s are equal.
+        /// </summary>
+        public static bool operator ==(FastColor left, FastColor right) => left.ToInt() == right.ToInt();
+
+        /// <summary>
+        /// Tests if <see cref="FastColor"/>s are not equal.
+        /// </summary>
+        public static bool operator !=(FastColor left, FastColor right) => left.ToInt() != right.ToInt();
+    }
+}
